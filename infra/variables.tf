df4b893205ada74785604413609e84d8afda032d
Mahindra <118008518+mahindragajjala@users.noreply.github.com>
2025-07-03 10:16:06 +0530

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for EC2 instances (Amazon Linux 2 or Ubuntu)"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # You can update this to latest
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "AWS Availability Zone"
+  type        = string
+  default     = "us-east-1a"
+}
