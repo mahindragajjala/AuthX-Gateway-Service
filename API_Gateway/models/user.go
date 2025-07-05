a68819533a1eff722cecf1df35ebad1baa3a32e0
Mahindra <118008518+mahindragajjala@users.noreply.github.com>
2025-07-05 12:01:43 +0530

    Delete API_Gateway directory

--- API_Gateway/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-// Input validation struct
-type SignupRequest struct {
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
-}
